56d661c392b332b2d86e2d28c18b69852524a3d4
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-06-22 09:44:55 -0700

    Catch an actual bug using Clang + GCC

--- test/driver.sh
+++ test/driver.sh
@@ -6,7 +6,9 @@ FSTAR_HOME="$(dirname $(which fstar.exe))/.."
 HYPERSTACK_LIB="$FSTAR_HOME/examples/low-level/ulib/hyperstack/"
 FSTAR_OPTIONS="--lax --trace_error --universes --codegen Kremlin"
 FSTAR="fstar.exe --include $HYPERSTACK_LIB $FSTAR_OPTIONS"
-CC="clang -fsanitize=undefined,integer -Wall -Wno-shift-op-parentheses \
+CLANG="clang -fsanitize=undefined,integer -Wall -Wno-shift-op-parentheses \
+  -Wno-unused-variable -Werror"
+GCC="gcc-5 -Wall -Wno-shift-op-parentheses \
   -Wno-unused-variable -Werror"
 
 FILES=Chacha01
@@ -14,7 +16,10 @@ FILES=Chacha01
 for f in $FILES; do
   $FSTAR $f.fst
   ../Kremlin.native -write out.krml
-  $CC $f.c main.c -o $f
+  $CLANG $f.c main.c -o $f
   ./$f
-  echo "$f exited with $?"
+  echo "$f/clang exited with $?"
+  $GCC $f.c main.c -o $f
+  ./$f
+  echo "$f/gcc exited with $?"
 done
