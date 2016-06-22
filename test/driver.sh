2e94edd1d1a565f7757461a08529fe6380d8587a
Jonathan Protzenko <protz@microsoft.com>
2016-06-22 11:53:09 -0700

    Make test driver work on Windows/cygwin

--- test/driver.sh
+++ test/driver.sh
@@ -2,23 +2,47 @@
 
 set -e
 
+# Path-conversion for Windows
 FSTAR_HOME="$(dirname $(which fstar.exe))/.."
+if which cygpath >/dev/null 2>&1; then
+  FSTAR_HOME=$(cygpath -m $FSTAR_HOME)
+fi
+
+# Couldn't get clang to work on Windows...
+if which clang >/dev/null 2>&1; then
+  HAS_CLANG=true
+else
+  HAS_CLANG=false
+fi
+
+# On OSX, actual GCC is gcc-5; on other systems, gcc gets some recent gcc.
+if which gcc-5 >/dev/null 2>&1; then
+  GCC=gcc-5
+elif which x86_64-w64-mingw32-gcc >/dev/null 2>&1; then
+  GCC=x86_64-w64-mingw32-gcc
+else
+  GCC=gcc
+fi
+echo GCC is $GCC
+  
+
 HYPERSTACK_LIB="$FSTAR_HOME/examples/low-level/ulib/hyperstack/"
 FSTAR_OPTIONS="--lax --trace_error --universes --codegen Kremlin"
 FSTAR="fstar.exe --include $HYPERSTACK_LIB $FSTAR_OPTIONS"
-CLANG="clang -fsanitize=undefined,integer -Wall -Wno-shift-op-parentheses \
-  -Wno-unused-variable -Werror"
-GCC="gcc-5 -Wall -Wno-shift-op-parentheses \
-  -Wno-unused-variable -Werror"
+OPTS="-Wall -Werror -Wno-parentheses -Wno-unused-variable"
+CLANG="clang -fsanitize=undefined,integer $OPTS"
+GCC="$GCC $OPTS"
 
 FILES=Chacha01
 
 for f in $FILES; do
   $FSTAR $f.fst
   ../Kremlin.native -write out.krml
-  $CLANG $f.c main.c -o $f
-  ./$f
-  echo "$f/clang exited with $?"
+  if $HAS_CLANG; then
+    $CLANG $f.c main.c -o $f
+    ./$f
+    echo "$f/clang exited with $?"
+  fi
   $GCC $f.c main.c -o $f
   ./$f
   echo "$f/gcc exited with $?"
