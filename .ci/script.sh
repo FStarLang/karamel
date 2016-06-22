775577cf42c32e340158d7e7785d12c350b5ce3e
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-06-22 09:22:20 -0700

    Ah, OCamlbuild

--- .ci/script.sh
+++ .ci/script.sh
@@ -10,9 +10,9 @@ export PATH=/home/travis/build/FStarLang/kremlin:$PATH;
 export PATH=/home/travis/build/FStarLang/kremlin/fstar/bin:$PATH;
 export PATH=/home/travis/build/FStarLang/kremlin/$CLANG/bin:$PATH;
 
-echo "<$Z3>: -traverse" >> _tags
-echo "<\"$CLANG\">: -traverse" >> _tags
-echo "<fstar>: -traverse" >> _tags
+echo "\"$Z3\": -traverse" >> _tags
+echo "\"$CLANG\": -traverse" >> _tags
+echo "\"fstar\": -traverse" >> _tags
 
 echo -e "\e[31m=== Some info about the environment ===\e[0m"
 ocamlfind ocamlopt -config
