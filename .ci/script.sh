ee2d55552d8f3e6dfff6d8f41c229090527e9f39
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-06-22 07:02:14 -0700

    Try CI again

--- .ci/script.sh
+++ .ci/script.sh
@@ -8,6 +8,9 @@ export PATH=/home/travis/build/FStarLang/kremlin/$Z3/bin:$PATH;
 export PATH=/home/travis/build/FStarLang/kremlin:$PATH;
 export PATH=/home/travis/build/FStarLang/kremlin/fstar/bin:$PATH;
 
+echo "<$Z3>: -traverse" >> _tags
+echo "<fstar>: -traverse" >> _tags
+
 echo -e "\e[31m=== Some info about the environment ===\e[0m"
 ocamlfind ocamlopt -config
 
