532cdb1f2bc84adb1299a80bc031fff9850de4c6
Jonathan Protzenko <protz@microsoft.com>
2016-08-10 17:01:47 -0700

    Silence some warnings, still not going through

--- kremlib/kremlib.h
+++ kremlib/kremlib.h
@@ -1,7 +1,7 @@
 #include <inttypes.h>
 
 typedef __int128 FStar_UInt128_t;
-typedef void *Prims_pos, *Prims_nat;
+typedef void *Prims_pos, *Prims_nat, *FStar_Seq_seq;
 
 uint64_t FStar_UInt64_gte_mask(uint64_t x, uint64_t y);
 uint64_t FStar_UInt64_eq_mask(uint64_t x, uint64_t y);
