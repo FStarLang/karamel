cfde9d3ac008c98378a435f63c3e1e3fb50e105d
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-10-14 17:28:11 +0200

    Minimalistic example, with @polubelova

--- src/CStarToWasm.ml
+++ src/CStarToWasm.ml
@@ -11,5 +11,42 @@ let dummy_region =
 let dummy_phrase what =
   W.Source.({ at = dummy_region; it = what })
 
-let mk_module (_files: (string * program) list): W.Ast.module_ =
-  dummy_phrase W.Ast.empty_module
+exception Not_implemented
+
+let mk_stmt (stmt: stmt) =
+  match stmt with
+  | Return None ->
+      dummy_phrase W.Ast.Return
+  | _ ->
+      raise Not_implemented
+
+let mk_stmts (block: block) =
+  List.map mk_stmt block
+
+let mk_decl (decl: decl) =
+  match decl with
+  | Function (_ret_t, _name, _binders, body) ->
+      begin try
+        let body = mk_stmts body in
+        Some (dummy_phrase W.Ast.({ ftype = dummy_phrase 0l; locals = []; body }))
+      with Not_implemented ->
+        None
+      end
+
+  | Global _ ->
+      None
+
+  | Type _ ->
+      None
+
+  | External _ ->
+      None
+
+let mk_module (files: (string * program) list): W.Ast.module_ =
+  let funcs = KList.map_flatten (fun (_, decls) ->
+    KList.filter_map mk_decl decls
+  ) files in
+  dummy_phrase W.Ast.({
+    empty_module with
+    funcs
+  })
---
+++ test/SimpleWasm.fst
@@ -0,0 +1,14 @@
+module SimpleWasm
+open FStar.ST
+
+val f: unit -> Stack unit (fun _ -> true) (fun _ _ _ -> true)
+let f () =
+  ()
+
+val main: Int32.t -> FStar.Buffer.buffer (FStar.Buffer.buffer C.char) ->
+  ST.Stack Int32.t (fun _ -> true) (fun _ _ _ -> true)
+let main argc argv =
+  push_frame ();
+  f ();
+  pop_frame ();
+  C.exit_success
