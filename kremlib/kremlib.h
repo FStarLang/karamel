e1a5bc66b34c81149e5bc913386373e2e6e2a6bf
Jonathan Protzenko <protz@microsoft.com>
2017-01-02 15:07:33 +0100

    clang-format kremlib/*.{c,h} test/*.{c,h} -i

--- kremlib/kremlib.h
+++ kremlib/kremlib.h
@@ -4,13 +4,10 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-
-
-#include <string.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
-
 
 // For types and values from C.fsti that do not exactly have the same name as
 // their C counterparts
@@ -28,14 +25,14 @@ typedef int8_t FStar_Int8_t, FStar_Int8_t_;
 
 #if defined(__GNUC__) && defined(__SIZEOF_INT128__)
 typedef unsigned __int128 FStar_UInt128_t, FStar_UInt128_t_;
-#define FStar_UInt128_add(x,y) ((x) + (y))
-#define FStar_UInt128_mul(x,y) ((x) * (y))
-#define FStar_UInt128_add_mod(x,y) ((x) + (y))
-#define FStar_UInt128_sub(x,y) ((x) - (y))
-#define FStar_UInt128_sub_mod(x,y) ((x) - (y))
-#define FStar_UInt128_logand(x,y) ((x) & (y))
-#define FStar_UInt128_logor(x,y) ((x) | (y))
-#define FStar_UInt128_logxor(x,y) ((x) ^ (y))
+#define FStar_UInt128_add(x, y) ((x) + (y))
+#define FStar_UInt128_mul(x, y) ((x) * (y))
+#define FStar_UInt128_add_mod(x, y) ((x) + (y))
+#define FStar_UInt128_sub(x, y) ((x) - (y))
+#define FStar_UInt128_sub_mod(x, y) ((x) - (y))
+#define FStar_UInt128_logand(x, y) ((x) & (y))
+#define FStar_UInt128_logor(x, y) ((x) | (y))
+#define FStar_UInt128_logxor(x, y) ((x) ^ (y))
 #define FStar_UInt128_lognot(x) (~(x))
 #define FStar_UInt128_shift_left(x, y) ((x) << (y))
 #define FStar_UInt128_shift_right(x, y) ((x) >> (y))
@@ -43,7 +40,7 @@ typedef unsigned __int128 FStar_UInt128_t, FStar_UInt128_t_;
 #define FStar_Int_Cast_uint128_to_uint64(x) ((uint64_t)(x))
 #define FStar_UInt128_mul_wide(x, y) ((__int128)(x) * (y))
 
-#define FStar_UInt128_op_Hat_Hat(x,y) ((x) ^ (y))
+#define FStar_UInt128_op_Hat_Hat(x, y) ((x) ^ (y))
 
 #else
 typedef struct {
@@ -81,22 +78,20 @@ FStar_UInt128_t FStar_UInt128_eq_mask(FStar_UInt128_t x, FStar_UInt128_t y);
 FStar_UInt128_t FStar_UInt128_gte_mask(FStar_UInt128_t x, FStar_UInt128_t y);
 
 // Buffers (FIXME remove eqb!)
-#define FStar_Buffer_eqb(b1, b2, n) \
-  (memcmp((b1), (b2), (n)*sizeof((b1)[0])) == 0)
+#define FStar_Buffer_eqb(b1, b2, n)                                            \
+  (memcmp((b1), (b2), (n) * sizeof((b1)[0])) == 0)
 void FStar_Buffer_recall(void *x);
 
 // Some types that KreMLin has no special knowledge of; many of them appear in
 // signatures of ghost functions, meaning that it suffices to give them (any)
 // definition.
 typedef void *Prims_pos, *Prims_nat, *Prims_nonzero, *FStar_Seq_seq, *Prims_int,
-  *Prims_prop, *FStar_HyperStack_mem, *FStar_Set_set, *Prims_st_pre_h, *FStar_Heap_heap,
-        *Prims_all_pre_h, *FStar_TSet_set, *Prims_string, *Prims_list,
-        *FStar_Map_t,
-        *FStar_UInt63_t_, *FStar_Int63_t_,
-        *FStar_UInt63_t, *FStar_Int63_t,
-        *FStar_UInt_uint_t, *FStar_Int_int_t,
-        *FStar_HyperStack_stackref, *FStar_Bytes_bytes,
-        *FStar_HyperHeap_rid, *FStar_Heap_aref;
+    *Prims_prop, *FStar_HyperStack_mem, *FStar_Set_set, *Prims_st_pre_h,
+    *FStar_Heap_heap, *Prims_all_pre_h, *FStar_TSet_set, *Prims_string,
+    *Prims_list, *FStar_Map_t, *FStar_UInt63_t_, *FStar_Int63_t_,
+    *FStar_UInt63_t, *FStar_Int63_t, *FStar_UInt_uint_t, *FStar_Int_int_t,
+    *FStar_HyperStack_stackref, *FStar_Bytes_bytes, *FStar_HyperHeap_rid,
+    *FStar_Heap_aref;
 
 // Prims; all of the functions below abort;
 bool Prims_op_GreaterThanOrEqual(Prims_int x, Prims_int y);
@@ -117,28 +112,36 @@ void *Prims____Cons___tl(void *_);
 // Kremlin generates calls to KRML_EABORT and it's not possible (as far as I
 // know) to define an expression that has a "universal size" and aborts when
 // evaluated...
-#define KRML_EXIT \
-  do { \
-    printf("Unimplemented function at %s:%d\n", __FILE__, __LINE__); \
-    exit(254); \
+#define KRML_EXIT                                                              \
+  do {                                                                         \
+    printf("Unimplemented function at %s:%d\n", __FILE__, __LINE__);           \
+    exit(254);                                                                 \
   } while (0)
 
-#define KRML_EABORT \
-  (exit(252), 0)
+#define KRML_EABORT (exit(252), 0)
 
 // Stubs to make ST happy
 bool FStar_HyperStack_is_eternal_color(Prims_int x0);
 #define FStar_ST_op_Colon_Equals(x, v) KRML_EXIT
 #define FStar_ST_op_Bang(x) 0
 #define FStar_ST_salloc(x) 0
-#define FStar_ST_ralloc(x,y) 0
+#define FStar_ST_ralloc(x, y) 0
 #define FStar_ST_new_region(x) 0
-#define FStar_ST_recall(x) do {} while (0)
-#define FStar_ST_recall_region(x) do {} while (0)
+#define FStar_ST_recall(x)                                                     \
+  do {                                                                         \
+  } while (0)
+#define FStar_ST_recall_region(x)                                              \
+  do {                                                                         \
+  } while (0)
 
-#define FStar_Monotonic_RRef_m_recall(...) do {} while (0)
-#define FStar_Monotonic_RRef_m_write(...) do {} while (0)
-#define FStar_Monotonic_RRef_m_alloc(...) { 0 }
+#define FStar_Monotonic_RRef_m_recall(...)                                     \
+  do {                                                                         \
+  } while (0)
+#define FStar_Monotonic_RRef_m_write(...)                                      \
+  do {                                                                         \
+  } while (0)
+#define FStar_Monotonic_RRef_m_alloc(...)                                      \
+  { 0 }
 
 #define FStar_HyperHeap_root 0
 
@@ -159,7 +162,6 @@ FStar_Seq_seq FStar_Seq_slice(FStar_Seq_seq x, FStar_Seq_seq y, Prims_nat z);
 #define FStar_Seq_index(x, y) 0
 FStar_UInt32_t FStar_UInt32_uint_to_t(Prims_nat x);
 
-
 // Endian-ness
 
 #if defined(__linux__) || defined(__CYGWIN__)
@@ -177,15 +179,15 @@ FStar_UInt32_t FStar_UInt32_uint_to_t(Prims_nat x);
 #define le16toh(x) OSSwapLittleToHostInt16(x)
 #define htobe16(x) OSSwapHostToBigInt16(x)
 #define be16toh(x) OSSwapBigToHostInt16(x)
- 
+
 #define htole32(x) OSSwapHostToLittleInt32(x)
 #define le32toh(x) OSSwapLittleToHostInt32(x)
 #define htobe32(x) OSSwapHostToBigInt32(x)
 #define be32toh(x) OSSwapBigToHostInt32(x)
 
-#elif (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
+#elif (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) &&               \
+    !defined(__WINDOWS__)
 #include <windows.h>
-
 
 #if BYTE_ORDER == LITTLE_ENDIAN
 
@@ -231,12 +233,12 @@ FStar_UInt32_t FStar_UInt32_uint_to_t(Prims_nat x);
 #define htole16(x) __builtin_bswap16(x)
 #define be16toh(x) (x)
 #define le16toh(x) __builtin_bswap16(x)
- 
+
 #define htobe32(x) (x)
 #define htole32(x) __builtin_bswap32(x)
 #define be32toh(x) (x)
 #define le32toh(x) __builtin_bswap32(x)
- 
+
 #define htobe64(x) (x)
 #define htole64(x) __builtin_bswap64(x)
 #define be64toh(x) (x)
@@ -246,14 +248,13 @@ FStar_UInt32_t FStar_UInt32_uint_to_t(Prims_nat x);
 
 #endif
 
-#define load64_le(b) (le64toh(*((uint64_t*) b)))
-#define store64_le(b,i) (*((uint64_t*)b)=(htole64(i)))
-#define load64_be(b) (be64toh(*((uint64_t*) b)))
-#define store64_be(b,i) (*((uint64_t*)b)=(htobe64(i)))
+#define load64_le(b) (le64toh(*((uint64_t *)b)))
+#define store64_le(b, i) (*((uint64_t *)b) = (htole64(i)))
+#define load64_be(b) (be64toh(*((uint64_t *)b)))
+#define store64_be(b, i) (*((uint64_t *)b) = (htobe64(i)))
 
 #define FStar_Buffer_to_seq_full(x) 0
 
 #undef force_inline
 #define force_inline __attribute__((always_inline))
 #endif
-
--- test/testlib.h
+++ test/testlib.h
@@ -1,11 +1,11 @@
 #ifndef __TESTLIB_H
 #define __TESTLIB_H
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 
 // This file has a hand-written .h file so that test files written in C (e.g.
 // main-Poly1305.c) can use the functions from this file too (e.g.
@@ -16,7 +16,8 @@ void TestLib_touch(int32_t);
 void TestLib_check(int32_t, int32_t);
 
 // These functions are also called by HACL
-void TestLib_compare_and_print(const char *txt, uint8_t *reference, uint8_t *output, int size);
+void TestLib_compare_and_print(const char *txt, uint8_t *reference,
+                               uint8_t *output, int size);
 
 void *TestLib_unsafe_malloc(size_t size);
 void TestLib_print_clock_diff(clock_t t1, clock_t t2);
@@ -28,13 +29,11 @@ void TestLib_perr(unsigned int err_code);
 
 typedef unsigned long long cycles;
 
-static __inline__ cycles TestLib_cpucycles(void)
-{
+static __inline__ cycles TestLib_cpucycles(void) {
   unsigned hi, lo;
-  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-  return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+  return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
 }
 void TestLib_print_cycles_per_round(cycles c1, cycles c2, uint32_t rounds);
-
 
 #endif
