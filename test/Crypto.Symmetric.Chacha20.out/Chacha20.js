721d623ef08ddd63ee067bc69e8adda7542f2ef2
Jonathan Protzenko <protz@microsoft.com>
2017-02-07 15:56:16 -0800

    Link Buffer_Utils and Chacha20

---
+++ test/Crypto.Symmetric.Chacha20.out/Chacha20.js
@@ -0,0 +1,20 @@
+// Just a wrapper around the generated Web Assembly
+let buf1 = readbuffer("Buffer_Utils.wasm");
+let buf2 = readbuffer("Crypto_Symmetric_Chacha20.wasm");
+print("read two files");
+
+let imports = {};
+WebAssembly.instantiate(buf1, imports).then(({ module, instance }) => {
+  print("Buffer_Utils ok");
+  print("Exports: "+Object.keys(instance.exports));
+  imports.Buffer_Utils = {};
+  for (let o of Object.keys(instance.exports)) {
+    imports.Buffer_Utils[o] = instance.exports[o];
+  }
+  return WebAssembly.instantiate(buf2, imports);
+}).then(({ module, instance }) => {
+  print("Crypto_Symmetric_Chacha20 ok");
+  print("Exports: "+Object.keys(instance.exports));
+}, (e) => {
+  print(e);
+});
