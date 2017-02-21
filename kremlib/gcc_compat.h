fade08bf28c1850d2ad2fc5b47e2379b9cf3995d
BarryBo <barrybo@microsoft.com>
2017-02-21 11:29:32 -0800

    Fix the Vale build onx 64 Linux by definining empty calling convention keywords

--- kremlib/gcc_compat.h
+++ kremlib/gcc_compat.h
@@ -4,11 +4,15 @@
 #ifdef __GNUC__
 // gcc does not support the __cdecl, __stdcall or __fastcall notation
 // except on Windows
-#ifndef _WIN32
+#ifdef _WIN32
 #define __cdecl __attribute__((cdecl))
 #define __stdcall __attribute__((stdcall))
 #define __fastcall __attribute__((fastcall))
-#endif // ! _WIN32
+#else
+#define __cdecl
+#define __stdcall
+#define __fastcall
+#endif // _WIN32
 #endif // __GNUC__
 
 #endif // __GCC_COMPAT_H
