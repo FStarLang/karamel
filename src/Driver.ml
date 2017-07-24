f3252ee0d8e072b7bd8a6c5af4a3b6c93465f7d8
Guido Martínez <mtzguido@gmail.com>
2017-07-24 18:28:29 -0300

    Driver: don't extract FStar.Map

--- src/Driver.ml
+++ src/Driver.ml
@@ -211,7 +211,7 @@ let detect_fstar () =
   List.iter record_no_extract
     [ "Int8"; "UInt8"; "Int16"; "UInt16"; "Int31"; "UInt31"; "Int32"; "UInt32";
       "Int63"; "UInt63"; "Int64"; "UInt64"; "Int128"; "Seq.Base"; "HyperStack.ST";
-      "HyperStack"; "HyperHeap"; "Math.Lib" ];
+      "HyperStack"; "HyperHeap"; "Math.Lib"; "Map" ];
   if not !Options.uint128 then
     fstar_options := (!fstar_home ^^ "ulib" ^^ "FStar.UInt128.fst") :: !fstar_options;
   KPrint.bprintf "%sfstar is:%s %s %s\n" Ansi.underline Ansi.reset !fstar (String.concat " " !fstar_options);
