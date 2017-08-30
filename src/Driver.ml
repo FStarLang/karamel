6d5f324c7231f053052b833e8496be83be48b316
Jared Roesch <roeschinc@gmail.com>
2017-08-30 16:25:10 -0700

    Do no extract FStar.UInt128
    For some reason UInt128 has recently started extracting even though it has support
    in Kremlin, ignore this module during extraction.

--- src/Driver.ml
+++ src/Driver.ml
@@ -210,7 +210,7 @@ let detect_fstar () =
   in
   List.iter record_no_extract
     [ "Int8"; "UInt8"; "Int16"; "UInt16"; "Int31"; "UInt31"; "Int32"; "UInt32";
-      "Int63"; "UInt63"; "Int64"; "UInt64"; "Int128"; "Seq.Base"; "HyperStack.ST";
+      "Int63"; "UInt63"; "Int64"; "UInt64"; "Int128"; "UInt128"; "Seq.Base"; "HyperStack.ST";
       "HyperStack"; "HyperHeap"; "Math.Lib"; "Map"; "Monotonic.HyperHeap"; "Buffer";
       "Monotonic.HyperStack" ];
   if not !Options.uint128 then
