a5235732fea1640f44388a1a158ca4d03b682766
Jared Roesch <roeschinc@gmail.com>
2017-09-29 11:44:49 -0700

    Restore bytes location for build

--- kremlib/kremlib.h
+++ kremlib/kremlib.h
@@ -497,28 +497,6 @@ static inline uint128_t FStar_UInt128_gte_mask(uint128_t x, uint128_t y) {
 /* This is a bad circular dependency... should fix it properly. */
 #include "FStar.h"
 
-// This is a hack, not sure how to work around cyclic dep.
-
-/* Bytes */
-typedef void* FStar_Bytes_bytes;
-
-extern FStar_Bytes_bytes FStar_Bytes_create(FStar_UInt32_t x, uint8_t y);
-extern krml_checked_int_t FStar_Bytes_len(FStar_Bytes_bytes bs);
-extern K___FStar_Bytes_bytes_FStar_Bytes_bytes FStar_Bytes_split(FStar_Bytes_bytes bs, FStar_UInt32_t i);
-extern FStar_Bytes_bytes FStar_Bytes_append(FStar_Bytes_bytes b1, FStar_Bytes_bytes b2);
-extern FStar_Bytes_bytes FStar_Bytes_xor(FStar_UInt32_t x, FStar_Bytes_bytes b1, FStar_Bytes_bytes b2);
-extern FStar_Bytes_bytes FStar_Bytes_bytes_of_int(krml_checked_int_t k, krml_checked_int_t n);
-extern krml_checked_int_t FStar_Bytes_int_of_bytes(FStar_Bytes_bytes bs);
-extern FStar_Bytes_bytes FStar_Bytes_twobytes(K___uint8_t_uint8_t two_bytes);
-extern int FStar_Bytes_get(FStar_Bytes_bytes bs, uint32_t i);
-extern int FStar_Bytes_repr_bytes(Prims_nat bs);
-extern FStar_Bytes_bytes FStar_Bytes_abyte(uint8_t byte);
-extern FStar_Bytes_bytes FStar_Bytes_utf8_encode(Prims_string str);
-extern FStar_Bytes_bytes FStar_Bytes_bytes_of_hex(Prims_string str);
-static FStar_Bytes_bytes FStar_Bytes_empty_bytes;
-
-/* End Bytes */
-
 typedef FStar_UInt128_uint128 FStar_UInt128_t_, uint128_t;
 
 /* A series of definitions written using pointers. */
@@ -579,8 +557,9 @@ static inline void store128_be(uint8_t *b, uint128_t n) { store128_be_(b, &n); }
 #endif /* KRML_STRUCT_PASSING */
 #endif /* KRML_UINT128 */
 
-// This is a hack, not sure how to work around cyclic dep.
 #include "FStar.h"
+
+// This is a hack, not sure how to work around cyclic dep.
 
 /* Bytes */
 typedef void* FStar_Bytes_bytes;
@@ -596,6 +575,9 @@ extern FStar_Bytes_bytes FStar_Bytes_twobytes(K___uint8_t_uint8_t two_bytes);
 extern int FStar_Bytes_get(FStar_Bytes_bytes bs, uint32_t i);
 extern int FStar_Bytes_repr_bytes(Prims_nat bs);
 extern FStar_Bytes_bytes FStar_Bytes_abyte(uint8_t byte);
+extern FStar_Bytes_bytes FStar_Bytes_utf8_encode(Prims_string str);
+extern FStar_Bytes_bytes FStar_Bytes_bytes_of_hex(Prims_string str);
+static FStar_Bytes_bytes FStar_Bytes_empty_bytes;
 
-
+/* End Bytes */
 #endif /* __KREMLIB_H */
