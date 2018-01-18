dc5a8be3a1b756492765d061511fb50c621715bb
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-18 15:11:16 -0800

    Mini Makefile

--- kremlib/C.ml
+++ kremlib/C.ml
@@ -1,11 +1,8 @@
-open FStar_Buffer
-
 type exit_code =
   | EXIT_SUCCESS
   | EXIT_FAILURE
 
-let exit_success = 0
-let exit_failure = 255
-
 let string_of_literal s = s
 let print_string = print_string
+
+let exit = exit
