db180f9996ad0be1ae8a7da897a36584ad317739
Barry Bond <barrybo@microsoft.com>
2018-02-03 13:04:34 -0800

    Replace calloc by KRML_HOST_CALLOC for portability.

--- kremlib/kremstr.c
+++ kremlib/kremstr.c
@@ -16,7 +16,7 @@ Prims_nat FStar_String_strlen(Prims_string s) {
 }
 
 Prims_string FStar_String_strcat(Prims_string s0, Prims_string s1) {
-  char *dest = calloc(strlen(s0) + strlen(s1) + 1, 1);
+  char *dest = KRML_HOST_CALLOC(strlen(s0) + strlen(s1) + 1, 1);
   strcat(dest, s0);
   strcat(dest, s1);
   return (Prims_string)dest;
@@ -51,7 +51,7 @@ krml_checked_int_t FStar_String_index_of(Prims_string s1, FStar_Char_char fc) {
 
 Prims_string FStar_String_substring(
     Prims_string s0, krml_checked_int_t from, krml_checked_int_t length) {
-  char *dest = calloc(length + 1, 1); // zero terminated
+  char *dest = KRML_HOST_CALLOC(length + 1, 1); // zero terminated
   strncpy(dest, s0 + from, length);
   return dest;
 }
