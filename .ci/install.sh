103bdff9ea277b191c8b66162779dff19017116b
Jonathan Protzenko <protz@microsoft.com>
2018-07-25 10:14:03 -0700

    See if I can speed up CI by a little bit

--- .ci/install.sh
+++ .ci/install.sh
@@ -8,12 +8,6 @@ git config --global user.email "everbld@microsoft.com"
 git clone https://github.com/fstarlang/fstar-mode.el
 git clone https://dzomo:$DZOMO_TOKEN@github.com/fstarlang/fstarlang.github.io
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  sudo apt-get install --yes libssl-dev opam libgmp-dev libsqlite3-dev g++-5 gcc-5 libc6-dev;
-  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 200;
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 200;
-fi
-
 sudo easy_install docutils sphinx sphinx-rtd-theme
 
 export OPAMYES=true
