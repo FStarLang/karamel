76808855ab389adffa93e7203e7e13c158e50bc8
Jonathan Protzenko <protz@microsoft.com>
2018-08-27 12:07:34 -0700

    Remove buggy eq_mask and gte_mask, now implemented and verified in ulib

--- kremlib/c/fstar_uint32.c
+++ kremlib/c/fstar_uint32.c
@@ -1,19 +1,5 @@
 #include "FStar_UInt32.h"
 
-uint32_t FStar_UInt32_eq_mask(uint32_t x, uint32_t y) {
-  x = ~(x ^ y);
-  x &= x << 16;
-  x &= x << 8;
-  x &= x << 4;
-  x &= x << 2;
-  x &= x << 1;
-  return ((int32_t)x) >> 31;
-}
-
-uint32_t FStar_UInt32_gte_mask(uint32_t x, uint32_t y) {
-  return ~((uint32_t)(((int64_t)x - y) >> 63));
-}
-
 Prims_string FStar_UInt32_to_string(uint32_t i) {
   char *buf = KRML_HOST_MALLOC(24);
   KRML_HOST_SNPRINTF(buf, 24, "%"PRIu32, i);
--- kremlib/c/fstar_uint8.c
+++ kremlib/c/fstar_uint8.c
@@ -1,17 +1,5 @@
 #include "FStar_UInt8.h"
 
-uint8_t FStar_UInt8_eq_mask(uint8_t x, uint8_t y) {
-  x = ~(x ^ y);
-  x &= x << 4;
-  x &= x << 2;
-  x &= x << 1;
-  return (int8_t)x >> 7;
-}
-
-uint8_t FStar_UInt8_gte_mask(uint8_t x, uint8_t y) {
-  return ~(uint8_t)(((int32_t)x - y) >> 31);
-}
-
 Prims_string FStar_UInt8_to_string(uint8_t i) {
   char *buf = KRML_HOST_MALLOC(24);
   KRML_HOST_SNPRINTF(buf, 24, "%"PRIu8, i);
