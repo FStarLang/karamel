5f5fb82fbbcf393f8e24da2c00d29fffa91102a9
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-10-08 14:51:34 -0700

    File not meant to be added... it should've been in the gitignore

--- src/AutoConfig.ml
+++
@@ -1,7 +0,0 @@
-(* Copyright (c) INRIA and Microsoft Corporation. All rights reserved. *)
-(* Licensed under the Apache 2.0 License. *)
-
-let kremlib_dir = "";;
-let runtime_dir = "";;
-let include_dir = "";;
-let misc_dir = "";;
