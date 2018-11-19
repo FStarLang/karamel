6f69c671883bd28722f1fb400a09c8caf53f191c
Christoph M. Wintersteiger <cwinter@microsoft.com>
2018-11-19 14:42:46 +0000

    Added implemenations for some FStar.Int.Cast functions.

---
+++ kremlib/c/fstar_int_cast.c
@@ -0,0 +1,14 @@
+/* Copyright (c) INRIA and Microsoft Corporation. All rights reserved.
+   Licensed under the Apache 2.0 License. */
+
+#include "FStar_Int_Cast.h"
+
+uint64_t FStar_Int_Cast_uint32_to_uint64(uint32_t x0)
+{
+    return (uint64_t) x0;
+}
+
+uint32_t FStar_Int_Cast_uint64_to_uint32(uint64_t x0)
+{
+    return (uint32_t) x0;
+}
