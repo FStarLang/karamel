d0fcbc6217f199ba56ba35fdeede8bb054926178
Jonathan Protzenko <protz@microsoft.com>
2018-11-28 09:52:32 +0000

    Fix an error message + encrypted (fresh) slack token

--- src/Bundles.ml
+++ src/Bundles.ml
@@ -208,8 +208,10 @@ let make_bundles files =
   let files = List.filter (fun (n, _) -> not (StringMap.mem n used)) files @ bundles in
 
   let names, _ = List.split files in
-  if List.length (List.sort_uniq compare names) <> List.length names then
-    KPrint.bprintf "Internal error, duplicate names after bundling\n";
+  let uniq_names = List.sort_uniq compare names in
+  if List.length uniq_names <> List.length names then
+    KPrint.bprintf "Internal error, duplicate names after bundling\n%s\n%s\n"
+      (String.concat " " (List.sort compare names)) (String.concat " " uniq_names);
 
   (* This is important, because bundling may creates cycles, that are broken
    * after removing (now-unused) functions. *)
