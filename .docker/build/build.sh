91cf7a296aef6ac9f58085a7cb4e72594b004463
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-02-07 13:27:32 -0800

    fix kremlin_home

--- .docker/build/build.sh
+++ .docker/build/build.sh
@@ -132,7 +132,7 @@ export OTHERFLAGS="--print_z3_statistics --use_hints --query_stats"
 export MAKEFLAGS="$MAKEFLAGS -Otarget"
 
 export_home FSTAR "$(pwd)/FStar"
-export_home KREMLIN "$(pwd)"
+export_home KREMLIN "$(pwd)/kremlin"
 
 cd kremlin
 rootPath=$(pwd)
