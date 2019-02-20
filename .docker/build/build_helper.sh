6bf5f89f38f68ff56103349cc7525e3128e83174
Gustavo Varo <gustavo.varo@microsoft.com>
2019-02-20 10:39:51 -0500

    fixing buildhelper file.

--- .docker/build/build_helper.sh
+++ .docker/build/build_helper.sh
@@ -5,17 +5,25 @@ out_file=$2
 threads=$3
 branchname=$4
 
+export FSTAR_HOME=$(pwd)/FStar
+
+# Add ssh identity
+eval $(ssh-agent)
+ssh-add .ssh/id_rsa
+
 eval $(opam config env)
 
 echo $(date -u "+%Y-%m-%d %H:%M:%S") >> $out_file
 
-# prefix every line on standard error with [STDERR]
 tail -f $out_file &
 tail_pd=$!
-#{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
+{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
 kill $tail_pd
 
 echo $(date -u "+%Y-%m-%d %H:%M:%S") >> $out_file
+
+eval $(ssh-agent)
+ssh-add -D
 
 # Generate query-stats.
 # List the hints that fail to replay.
