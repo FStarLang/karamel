e1d2e27e73991bc826e5e968e5c5baf491373b3b
Tahina Ramananandro <taramana@microsoft.com>
2019-05-15 14:18:43 -0700

    do not buffer output

--- .docker/build/build_helper.sh
+++ .docker/build/build_helper.sh
@@ -17,7 +17,8 @@ echo $(date -u "+%Y-%m-%d %H:%M:%S") >> $out_file
 
 tail -f $out_file &
 tail_pd=$!
-{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
+{ ./build.sh "$@" 2&>1 ; } | tee $out_file
+# { { { { { { stdbuf -e0 -o0 ./build.sh "$@" ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
 kill $tail_pd
 
 echo $(date -u "+%Y-%m-%d %H:%M:%S") >> $out_file
