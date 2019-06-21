e800dd7fe661265486bafe519dd6163ca9e4f239
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-06-20 18:53:28 -0700

    proper fix

--- src/Structs.ml
+++ src/Structs.ml
@@ -278,7 +278,7 @@ let pass_by_ref files =
   (pass_by_ref is_struct)#visit_files [] files
 
 let hidden_visibility = {|
-#if defined(__GNUC__) && (defined(_WIN32) || defined(_WIN64))
+#if defined(__GNUC__) && !(defined(_WIN32) || defined(_WIN64))
 __attribute__ ((visibility ("hidden")))
 #endif
 |}
