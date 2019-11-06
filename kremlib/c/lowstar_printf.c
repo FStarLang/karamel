1425b9187c5f43c25e8451347a4fa30f61d0f9b2
Santiago Zanella-Beguelin <santiago@microsoft.com>
2019-11-06 17:13:34 +0000

    LowStar.Printf: print buffer contents in hexadecimal format

--- kremlib/c/lowstar_printf.c
+++ kremlib/c/lowstar_printf.c
@@ -11,20 +11,20 @@
       printf("%" M " ", x[i]); \
   }
 
-#define PRINT2(N, T, M) \
-  PRINT(N, T, M) \
-  PRINTB(N, T, M)
+#define PRINT2(N, T, M1, M2) \
+  PRINT(N, T, M1) \
+  PRINTB(N, T, M2)
 
-PRINT2 (string, Prims_string, "s")
-PRINT2 (char, FStar_Char_char, PRIu32)
-PRINT2 (u8, uint8_t, PRIu8)
-PRINT2 (u16, uint16_t, PRIu16)
-PRINT2 (u32, uint32_t, PRIu32)
-PRINT2 (u64, uint64_t, PRIu64)
-PRINT2 (i8, int8_t, PRIi8)
-PRINT2 (i16, int16_t, PRIi16)
-PRINT2 (i32, int32_t, PRIi32)
-PRINT2 (i64, int64_t, PRIi64)
+PRINT2 (string, Prims_string, "s", "s")
+PRINT2 (char, FStar_Char_char, PRIu32, PRIx32)
+PRINT2 (u8, uint8_t, PRIu8, PRIx8)
+PRINT2 (u16, uint16_t, PRIu16, PRIx16)
+PRINT2 (u32, uint32_t, PRIu32, PRIx32)
+PRINT2 (u64, uint64_t, PRIu64, PRIx64)
+PRINT2 (i8, int8_t, PRIi8, PRIx8)
+PRINT2 (i16, int16_t, PRIi16, PRIx16)
+PRINT2 (i32, int32_t, PRIi32, PRIx32)
+PRINT2 (i64, int64_t, PRIi64, PRIx64)
 
 void LowStar_Printf_print_bool(bool b) {
   printf("%s", b ? "true" : "false");
