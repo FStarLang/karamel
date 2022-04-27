5eeb7e4a20b1297a918e6a6f260e186d805395e6
Tahina Ramananandro <taramana@microsoft.com>
2022-04-27 12:33:35 -0700

    honor F* branch

--- .docker/build/install-deps.sh
+++ .docker/build/install-deps.sh
@@ -3,15 +3,19 @@
 set -e
 set -x
 
+build_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # FIXME: the `opam depext` command should be unnecessary with opam 2.1
 opam depext conf-gmp z3.4.8.5 conf-m4
 
+# Identify the F* branch
+FSTAR_BRANCH=$(jq -c -r '.BranchName' "$build_home"/config.json)
+
 # Install F*
 [[ -n "$FSTAR_HOME" ]]
-git clone https://github.com/FStarLang/FStar "$FSTAR_HOME"
+git clone --branch $FSTAR_BRANCH https://github.com/FStarLang/FStar "$FSTAR_HOME"
 opam install --deps-only "$FSTAR_HOME/fstar.opam"
 OTHERFLAGS='--admit_smt_queries true' make -j 24 -C "$FSTAR_HOME"
 
 # Install other deps
-build_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$build_home"/install-other-deps.sh
