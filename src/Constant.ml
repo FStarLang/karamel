e6adb605f979bb06df4797a665e7990eb184646a
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-10-22 12:54:47 +0200

    Add size_t/ptrdiff_t to constants

--- src/Constant.ml
+++ src/Constant.ml
@@ -9,6 +9,7 @@ type t = width * string
 and width =
   | UInt8 | UInt16 | UInt32 | UInt64 | Int8 | Int16 | Int32 | Int64
   | Bool
+  | SizeT | PtrdiffT
   | CInt (** Checked signed integers. *)
 
 let bytes_of_width (w: width) =
@@ -44,11 +45,11 @@ let unsigned_of_signed = function
   | Int16 -> UInt16
   | Int32 -> UInt32
   | Int64 -> UInt64
-  | CInt | UInt8 | UInt16 | UInt32 | UInt64 | Bool -> raise (Invalid_argument "unsigned_of_signed")
+  | CInt | UInt8 | UInt16 | UInt32 | UInt64 | SizeT | PtrdiffT | Bool -> raise (Invalid_argument "unsigned_of_signed")
 
 let is_signed = function
-  | Int8 | Int16 | Int32 | Int64 | CInt -> true
-  | UInt8 | UInt16 | UInt32 | UInt64 -> false
+  | Int8 | Int16 | Int32 | Int64 | CInt | PtrdiffT -> true
+  | UInt8 | UInt16 | UInt32 | UInt64 | SizeT -> false
   | Bool -> raise (Invalid_argument "is_signed")
 
 let is_unsigned w = not (is_signed w)
