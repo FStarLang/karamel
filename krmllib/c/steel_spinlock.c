e1a65b656fa93ea2208376a6a9371f34b2bebaeb
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-11-09 13:49:56 -0800

    Use pthread for steel locks

--- krmllib/c/steel_spinlock.c
+++ krmllib/c/steel_spinlock.c
@@ -1,14 +1,17 @@
 #include "Steel_SpinLock.h"
-
+#include <pthread.h>
 
 Steel_SpinLock_lock_t Steel_SpinLock_new_lock () {
-  return true;
+  pthread_mutex_t mutex;
+  int rc = pthread_mutex_init(&mutex, NULL);
+  assert (rc = 0);
+  return mutex;
 }
 
 void Steel_SpinLock_acquire(Steel_SpinLock_lock_t l) {
-  return;
+  pthread_mutex_lock(&l); 
 }
 
 void Steel_SpinLock_release(Steel_SpinLock_lock_t l) {
-  return;
+  pthread_mutex_unlock(&l);
 }
