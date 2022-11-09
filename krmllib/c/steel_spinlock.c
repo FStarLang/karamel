e134113f61cd6a61b076ad518d227ce2a825e801
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-11-09 13:00:22 -0800

    Add handwritten implem of steel_spinlock

---
+++ krmllib/c/steel_spinlock.c
@@ -0,0 +1,14 @@
+#include "Steel_SpinLock.h"
+
+
+Steel_SpinLock_lock_t Steel_SpinLock_new_lock () {
+  return true;
+}
+
+void Steel_SpinLock_acquire(Steel_SpinLock_lock_t l) {
+  return;
+}
+
+void Steel_SpinLock_release(Steel_SpinLock_lock_t l) {
+  return;
+}
