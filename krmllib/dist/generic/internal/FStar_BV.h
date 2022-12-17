1f39590352a01484daca09a635300da6e6bbc018
Jonathan Protzenko <protz@microsoft.com>
2022-12-16 21:16:11 -0800

    One internal header appears

---
+++ krmllib/dist/generic/internal/FStar_BV.h
@@ -0,0 +1,21 @@
+/*
+  Copyright (c) INRIA and Microsoft Corporation. All rights reserved.
+  Licensed under the Apache 2.0 License.
+*/
+
+
+#ifndef __internal_FStar_BV_H
+#define __internal_FStar_BV_H
+
+
+
+#include "../FStar_BV.h"
+#include <inttypes.h>
+#include "krmllib.h"
+#include "krml/internal/compat.h"
+#include "krml/internal/target.h"
+Prims_list__bool *FStar_Seq_Base_slice___bool(Prims_list__bool *s, Prims_int i, Prims_int j);
+
+
+#define __internal_FStar_BV_H_DEFINED
+#endif
