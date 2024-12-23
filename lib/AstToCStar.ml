d3160da32174fb501452d8552643f5053d09874d
Jonathan Protzenko <protz@microsoft.com>
2024-12-23 10:44:57 -0800

    Remove debug

--- lib/AstToCStar.ml
+++ lib/AstToCStar.ml
@@ -487,7 +487,6 @@ and mk_stmts env e ret_type =
     | ELet (b, e1, e2) ->
         let env, binder = mk_and_push_binder env b Local (Some e1) [ e2 ]
         and e1 = mk_expr env false e1 in
-        KPrint.bprintf "pushed %s; now %s\n" b.node.name (String.concat ", " env.names);
         let acc = CStar.Decl (binder, e1) :: comment (e.meta @ b.meta) @ acc in
         collect (env, acc) return_pos e2
 
