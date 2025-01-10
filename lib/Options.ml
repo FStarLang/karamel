fb3f08e24b07b6f209c70d3415927bf9c2ac05e9
Guido Martínez <mtzguido@gmail.com>
2025-01-10 12:05:23 -0800

    Options: add CUDA mode to choose .cu extension

--- lib/Options.ml
+++ lib/Options.ml
@@ -40,6 +40,7 @@ let no_prefix: Bundle.pat list ref = ref Bundle.[
   Module [ "LowStar"; "Endianness" ]
 ]
 let fstar = ref "fstar.exe" (* F* command to use *)
+let cuda = ref false
 (* krmllib.h now added directly in Output.ml so that it appears before the first
  * #ifdef *)
 let add_include: (include_ * string) list ref = ref [ ]
