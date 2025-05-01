e241cd42a2ca9e442970d91982d086d5b08a8fed
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2025-05-01 18:51:12 -0400

    Add krml::unroll_for! to MiniRust printing environment

--- lib/PrintMiniRust.ml
+++ lib/PrintMiniRust.ml
@@ -666,6 +666,7 @@ let print_decl env d =
 
 let print_decls ns ds =
   let env = fresh ns in
+  let env, _ = register_global env ["krml"; "unroll_for!"] in
   let env, ds = List.fold_left (fun (env, ds) d ->
     let env, d = print_decl env d in
     env, d :: ds
